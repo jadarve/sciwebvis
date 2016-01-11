6640981ddcd02e74c96fa7a8ca38428bbf5ad6e4
Juan David Adarve Bermudez <juanda0718@gmail.com>
2016-01-12 00:26:10 +1100

    Changed URL of three.js to Google CDN

--- python/sciwebvis/figure.py
+++ python/sciwebvis/figure.py
@@ -184,10 +184,10 @@ class Figure(JSRenderable):
         # JavaScript code
         JScode = self.render()
 
-        libs = ['http://threejs.org/build/three.min.js',
+        libs = ['https://ajax.googleapis.com/ajax/libs/threejs/r69/three.min.js',
                 'http://threejs.org/examples/js/controls/OrbitControls.js',
-                'js/numjis_bundle.js',
-                'js/sciwis_bundle.js']
+                './js/numjis_bundle.js',
+                './js/sciwis_bundle.js']
         JS = display.Javascript(data = JScode, lib = libs)
         display.display(JS)
 
